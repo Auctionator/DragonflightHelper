6f6398d193e9ebf6b23a249fa18f24b07d2c5dfb
John Roberts <roberts.john@gmail.com>
2023-04-07 16:18:04 -0700

    Used plusmouse's code to fix timers :)

--- Source/Timers/Mixins/Timers.lua
+++ Source/Timers/Mixins/Timers.lua
@@ -29,79 +29,46 @@ end
 
 function DFH_TimerMixin:OnShow()
   self:Update()
-  self.timer = C_Timer.NewTicker(10, function() self:Update() end)
+  self.timer = C_Timer.NewTimer(30, function() self:Update() end)
 end
 
 function DFH_TimerMixin:OnHide()
   if self.timer ~= nil then
-    self.timer:Cancel()
-    self.timer = nil
+    -- self.timer:Cancel()
+    -- self.timer = nil
   end
 end
 
--- Stealing logic from https://wago.io/fVZJC8tik (remember to credit)
--- Mon Nov 21 2022 00:00:00 GMT+0100 (Central European Standard Time)
-local REGIONAL_WEEKLY_START = 1668981600
-local regionId = GetCurrentRegion()
-
-if regionId == 1 then
-  -- US (includes Brazil and Oceania)
-  REGIONAL_WEEKLY_START = REGIONAL_WEEKLY_START - 3 * 60 * 60
-elseif regionId == 2 then
-  -- Korea
-  REGIONAL_WEEKLY_START = REGIONAL_WEEKLY_START - 2 * 60 * 60
-elseif regionId == 3 then
-  -- Europe (includes Russia) - here for completeness
-elseif regionId == 4 then
-  -- Taiwan
-elseif regionId == 5 then
-  -- China
-end
-
 function DFH_TimerMixin:Update()
-  local eventIntervalInSeconds = self.frequency * 60 * 60
-  local secondsUntilNextEvent = (GetServerTime() - REGIONAL_WEEKLY_START) % eventIntervalInSeconds
+  local interval = self.frequency * 60 * 60
+  local secondsUntilNextEvent = (C_DateAndTime.GetSecondsUntilDailyReset() / interval) % 1 * interval
 
-  local aboutToStart = secondsUntilNextEvent <= 15 * 60
-  local inProgress = (eventIntervalInSeconds - secondsUntilNextEvent) <= 15 * 60
-
-  -- local hour, minute = GetGameTime()
-  -- local currentTime = (hour * 60) + minute
-
-  -- local howManyToday = math.floor((currentTime - (self.startTime * 60)) / (self.frequency * 60))
-  -- local lastStartTime = howManyToday * self.frequency * 60 + self.startTime * 60
-  -- local diff = currentTime - lastStartTime
-  -- local untilNext = self.frequency * 60 - diff
-  -- local aboutToStart = untilNext <= self.activeThreshold
-  -- local inProgress = diff < self.activeThreshold
-  -- DFH_Utilities.dump({
-  --   frequency = self.frequency,
-  --   howManyToday = howManyToday,
-  --   lastStartTime = lastStartTime,
-  --   diff = diff,
-  --   untilNext = untilNext,
-  --   aboutToStart = untilNext < 15 and "yes" or "no",
-  --   inProgress = diff < self.frequency * 60 - 15 and "yes" or "no",
-  --   remaining = self.frequency * 60 - diff
-  -- }, self.title)
-
+  local aboutToStart = secondsUntilNextEvent <= (15 * 60)
+  local inProgress = math.abs(interval - secondsUntilNextEvent) <= (self.activeThreshold * 60)
 
   if aboutToStart then
     self:SetForegroundColor(self.color.r, self.color.g, self.color.b)
-    self:SetMinMaxValues(0, self.frequency * 60)
-    self:SetValue(secondsUntilNextEvent)
+    self:SetMinMaxValues(0, interval)
+    self:SetValue(interval - secondsUntilNextEvent)
     self:SetDescription("About to start - " .. SecondsToTime(secondsUntilNextEvent))
   elseif inProgress then
     self:SetForegroundColor(self.thresholdColor.r, self.thresholdColor.g, self.thresholdColor.b)
     self:SetMinMaxValues(0, self.activeThreshold * 60)
-    self:SetValue(self.activeThreshold * 60 - (eventIntervalInSeconds - secondsUntilNextEvent))
+    self:SetValue(secondsUntilNextEvent - self.activeThreshold * 60)
     self:SetDescription("Active - " ..
-      SecondsToTime((self.activeThreshold * 60 - (eventIntervalInSeconds - secondsUntilNextEvent)) * 60))
+      SecondsToTime((secondsUntilNextEvent - self.activeThreshold * 60), true))
   else
+    DFH_Utilities.info(self.title, interval, secondsUntilNextEvent)
     self:SetForegroundColor(self.color.r, self.color.g, self.color.b)
-    self:SetMinMaxValues(0, self.frequency * 60)
+    self:SetMinMaxValues(0, interval)
     self:SetValue(secondsUntilNextEvent)
-    self:SetDescription(SecondsToTime(secondsUntilNextEvent))
+    self:SetDescription(SecondsToTime(secondsUntilNextEvent, true))
+  end
+
+  if secondsUntilNextEvent < (5 * 60) then
+    self.timer = C_Timer.NewTimer(1, function() self:Update() end)
+  else
+    self.timer = C_Timer.NewTimer(30, function() self:Update() end)
   end
 end
 
